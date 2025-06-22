da0bed2111bc1e86075656072dc663536f05d1a3
GhosTHaise <ghostrex2@gmail.com>
2025-06-22 20:31:18 +0300

    feat : install tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <div>
-      s
+      <h1 className="text-3xl text-indigo-300">Hello Gsap</h1>
     </div>
   )
 }
