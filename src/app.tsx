710c3d138417b0814ab961105fa182107884c26d
GhosTHaise <ghostrex2@gmail.com>
2025-06-23 22:05:15 +0300

    feat : constants

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,15 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/navbar";
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-indigo-300">Hello Gsap</h1>
-    </div>
+    <main className="">
+      <Navbar />
+    </main>
   )
 }
 
