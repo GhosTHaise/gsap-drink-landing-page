212606786f024d499e3204d3a433b4a8e76fbc0d
GhosTHaise <ghostrex2@gmail.com>
2025-06-23 22:18:13 +0300

    fix  logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,6 +8,7 @@ const Navbar = () => {
                     href="#home"
                     className="flex items-center gap-2"
                 >
+                    <img src="/images/logo.png" alt="logo" />
                     <p>
                         Velvet Pour
                     </p>
