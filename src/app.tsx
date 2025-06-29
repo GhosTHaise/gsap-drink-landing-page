45e364824534bcf29b8a8b8994a520b1c3d077bd
GhosTHaise <ghostrex2@gmail.com>
2025-06-29 20:06:03 +0300

    feat : leaf animation

--- src/app.tsx
+++ src/app.tsx
@@ -13,7 +13,6 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
-      <div className="h-dvh bg-black"></div>
     </main>
   )
 }
--- src/components/cocktails.tsx
+++ src/components/cocktails.tsx
@@ -1,6 +1,23 @@
+import { useGSAP } from "@gsap/react"
 import { cocktailLists, mockTailLists } from "../constants"
+import gsap from "gsap"
 
 const Cocktails = () => {
+    useGSAP(() => {
+        const parallaxTimeline = gsap.timeline({
+            scrollTrigger : {
+                trigger : "#cocktails",
+                start : "top 30%",
+                end : "bottom 80%",
+                scrub : true
+            }
+        });
+
+        parallaxTimeline
+        .from("#c-left-leaf", {x : -100, y : 100})
+        .from("#c-right-leaf", {x : 100, y : 100})
+    },[])
+
     return (
         <section id="cocktails" className="noisy">
             <img
